24324714dd66152f5eb0480003f9f56a2de92768
Matheus <matheus.malmeida7@gmail.com>
2021-10-06 13:46:24 -0300

    [Feature Matheus - Task:Implements Models of usuario and UsuarioLogin]

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -0,0 +1,47 @@
+package org.generation.blogPessoal.model;
+
+public class UserLogin {
+	
+	private String nome;
+	
+	private String usuario;
+	
+	private String Senha;
+	
+	private String token;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return Senha;
+	}
+
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
